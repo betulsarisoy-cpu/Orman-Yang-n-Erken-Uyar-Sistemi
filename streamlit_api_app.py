38c3ac04a2550f1f1a485492f41c7202f039afdd
betulatassarisoy <betulatas07@gmail.com>
2026-09-16 15:39:51 +0300

    render streamlite bağlandı

--- streamlit_api_app.py
+++ streamlit_api_app.py
@@ -19,10 +19,7 @@ import requests
 import os
 import streamlit as st
 
-API_URL = "https://akdeniz-yangin-api.onrender.com"API_URL = os.getenv(
-    "YANGIN_API_URL",
-    "https://akdeniz-yangin-api.onrender.com",
-).rstrip("/")
+API_URL = "https://akdeniz-yangin-api.onrender.com"
 
 # ============================================================
 # 1. SAYFA AYARLARI
